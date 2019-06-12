2409d498ea260c9c1a2766e957b95d629608829b
Claudia Solbes <cssolbes@hotmail.com>
2019-06-12 01:37:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,7 +5,7 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   return the_manifest
 end
 
@@ -26,7 +26,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest["muskets"] = "2"
-  shipping_manifest["gun powder"] = "4"
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
   puts shipping_manifest
 end
